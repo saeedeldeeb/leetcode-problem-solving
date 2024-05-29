3dd669ba28beafdbf1ce938b7f72a4e9828c650d
eldeeb <saeedeldeeb1@gmail.com>
2024-05-29 11:32:21 +0300

    f as index not value

--- remove_duplicates_from_sorted_array.go
+++ remove_duplicates_from_sorted_array.go
@@ -1,14 +1,14 @@
 package main
 
 func removeDuplicates(nums []int) int {
-	f := nums[0]
+	f := 0
 	for i := 1; i < len(nums); {
-		if f == nums[i] {
+		if nums[f] == nums[i] {
 			nums = append(nums[:i], nums[i+1:]...)
 			continue
 		}
 		i++
-		f = nums[i-1]
+		f = i - 1
 	}
 	return len(nums)
 }
